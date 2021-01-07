759484d59c15a587c3cf9a017ead2dbc1974e76a
Thiago Falcão da Silva <thiago.falcao86@gmail.com>
2021-01-06 21:43:54 -0300

    Update #3
    Updating text and identation of codes!

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,17 @@
+/*jQuery de setup do carousel*/
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:5
+        1000: {
+            items: 5
         }
     }
 })
